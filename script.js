380df261279eeaa9bae5c6c5aea9a7496ad82622
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-20 19:27:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-var dateDiffInDays = function (date1, date2) {
-  //   write your code here
-};
+function dateDiffInDays(date1, date2) {
+  const utcDate1 = Date.UTC(
+    parseInt(date1.substring(0, 4)),
+    parseInt(date1.substring(5, 7)) - 1,
+    parseInt(date1.substring(8))
+  );
+  const utcDate2 = Date.UTC(
+    parseInt(date2.substring(0, 4)),
+    parseInt(date2.substring(5, 7)) - 1,
+    parseInt(date2.substring(8))
+  );
 
-// Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+  const diffInMilliseconds = utcDate2 - utcDate1;
+  const diffInDays = diffInMilliseconds / (24 * 60 * 60 * 1000);
+
+  return diffInDays;
+}
+
+function calculateDateDifference() {
+  const date1 = document.getElementById('date1').value;
+  const date2 = document.getElementById('date2').value;
+
+  const result = dateDiffInDays(date1, date2);
+
+  document.getElementById('result').innerText = `The difference in days is: ${result}`;
+}
+
